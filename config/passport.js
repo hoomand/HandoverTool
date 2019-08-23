43a64b5139ad5d81187fa632ea0d2b00257848de
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-08-23 11:46:54 +1000

    conf(passport): add authentication lib

---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("../../models/User");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "secret";
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.get({ alias: jwt_payload.alias }, (err, user) => {
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      });
+    })
+  );
+};
