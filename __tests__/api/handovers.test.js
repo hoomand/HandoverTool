71f1ae6d2b627d3513a85dbf1168f979da31afd1
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-09-10 17:14:29 +1000

    test(Handover): add Handover test with minimal test conditions

---
+++ __tests__/api/handovers.test.js
@@ -0,0 +1,73 @@
+const request = require("supertest");
+const app = require("../../app");
+const User = require("../../models/User");
+const Team = require("../../models/Team");
+const { createUser, loginUser, createTeam } = require("../utils");
+const randomText = require("../../utils");
+
+let test_user1 = {
+  alias: "handover_test_user",
+  password: randomText(10),
+  token: ""
+};
+
+const test_teams = [{ name: "handover_team_1" }, { name: "handover_team_2" }];
+
+beforeAll(async () => {
+  const { alias, password } = test_user1;
+  await createUser(alias, password);
+  test_user1.token = await loginUser(alias, password);
+
+  await createTeam(test_teams[0].name, test_user1.token);
+  await createTeam(test_teams[1].name, test_user1.token);
+});
+
+afterAll(async () => {
+  await User.batchDelete([{ alias: test_user1.alias }]);
+  await Team.batchDelete([
+    { name: test_teams[0].name },
+    { name: test_teams[1].name }
+  ]);
+});
+
+describe("POST /api/handovers", () => {
+  test("It should fail creating new handover without authentication", async () => {
+    const response = await request(app)
+      .post("/api/handovers")
+      .send({
+        handingOverTeam: test_teams[0].name,
+        handedOverTeam: test_teams[1].name
+      });
+    expect(response.text).toEqual("Unauthorized");
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("It should successfully save a new handover", async () => {
+    const { token } = test_user1;
+    const response = await request(app)
+      .post("/api/handovers")
+      .send({
+        userAlias: test_user1.name,
+        handingOverTeam: test_teams[0].name,
+        handedOverTeam: test_teams[1].name,
+        items: [
+          {
+            action: "solve it",
+            link: "http://blahblah.com",
+            description: "oh lala"
+          },
+          {
+            action: "just close",
+            link: "http://blahblah.com",
+            description: "oh lala"
+          }
+        ]
+      })
+      .set({
+        "Content-Type": "application/json",
+        Authorization: token
+      });
+
+    expect(response.statusCode).toBe(200);
+  });
+});
