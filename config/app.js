54543e64646b35bdec8c4b60e08e00c8e3f4dc20
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-09-16 14:10:47 +1000

    conf(handover): add validStatuses & item type config

--- config/app.js
+++ config/app.js
@@ -1,3 +1,13 @@
 module.exports = {
-  aliasIsEmail: false
+  aliasIsEmail: false,
+  handoverItem: {
+    validStatuses: {
+      fresh: "fresh issue, no investigation done yet",
+      investigated: "did some investigation, but couldn't fully diagnose",
+      diagnosed: "fully diagnosed, clear steps provided to rectify the issue",
+      monitor:
+        "diagnosed actions were performed, only monitoring needed to confirm issue is rectified"
+    },
+    mustBeValidURL: true
+  }
 };
