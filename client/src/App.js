7e7f1db72b5914587900ba78a817175e5ed9a52e
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-09-24 18:07:22 +1000

    conf: add material-ui react library

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import "typeface-roboto";
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
   render() {
-    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+    return (
+      <div>
+        I'M READY TO USE THE BACK END APIS! :-)
+        <Button variant="contained" color="primary">
+          Ok?!
+        </Button>
+      </div>
+    );
   }
 }
 
