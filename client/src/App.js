2583e080917653be2927bf0560f0ac7e45406d65
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-10-02 08:35:45 +1000

    feat(login): check login status in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { setCurrentUser, logoutUser } from "./redux/actions/authActions";
 import store from "./redux/store";
 import "typeface-roboto";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Paperbase from "./components/layout/Paperbase";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info & exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
+  }
+}
 class App extends Component {
   render() {
     return (
