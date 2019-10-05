31114d6c4a9db4dde56be1cf5e2d65d9be4241b4
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-10-06 08:03:33 +1100

    ref(Header): show different avatar for logged in/anonymous user

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,23 +1,17 @@
-import React from "react";
-import clsx from "clsx";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { loadCSS } from "fg-loadcss";
 import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import HelpIcon from "@material-ui/icons/Help";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
-import Link from "@material-ui/core/Link";
 import MenuIcon from "@material-ui/icons/Menu";
-import NotificationsIcon from "@material-ui/icons/Notifications";
 import Toolbar from "@material-ui/core/Toolbar";
-import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
 import { withStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
-import FaceIcon from "@material-ui/icons/Face";
 
 const lightColor = "rgba(255, 255, 255, 0.7)";
 
@@ -43,64 +37,70 @@ const styles = theme => ({
   }
 });
 
-function Header(props) {
-  const { classes, onDrawerToggle } = props;
-
-  React.useEffect(() => {
+class Header extends Component {
+  componentDidMount() {
     loadCSS(
       "https://use.fontawesome.com/releases/v5.1.0/css/all.css",
       document.querySelector("#font-awesome-css")
     );
-  }, []);
+  }
+  render() {
+    const { classes, onDrawerToggle } = this.props;
+    const { isAuthenticated, user } = this.props.auth;
 
-  return (
-    <React.Fragment>
-      <AppBar color="primary" position="sticky" elevation={0}>
-        <Toolbar>
-          <Grid container spacing={1} alignItems="center">
-            <Hidden smUp>
-              <Grid item>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={onDrawerToggle}
-                  className={classes.menuButton}
-                >
-                  <MenuIcon />
-                </IconButton>
+    const avatar = isAuthenticated ? (
+      <Icon className="fas fa-user-circle" />
+    ) : (
+      <Tooltip title="anonymous user">
+        <Icon className="fas fa-user-secret" />
+      </Tooltip>
+    );
+
+    return (
+      <React.Fragment>
+        <AppBar color="primary" position="sticky" elevation={0}>
+          <Toolbar>
+            <Grid container spacing={1} alignItems="center">
+              <Hidden smUp>
+                <Grid item>
+                  <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={onDrawerToggle}
+                    className={classes.menuButton}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                </Grid>
+              </Hidden>
+              <Grid item xs />
+              <Grid item>{avatar}</Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <AppBar
+          component="div"
+          className={classes.secondaryBar}
+          color="primary"
+          position="static"
+          elevation={0}
+        >
+          <Toolbar>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item xs>
+                <Typography color="inherit" variant="h5" component="h1">
+                  Authentication
+                </Typography>
               </Grid>
-            </Hidden>
-            <Grid item xs />
-            <Grid item>
-              {/* <FaceIcon /> */}
-              <Icon className="fas fa-user-circle" />
-              <Icon className="fas fa-question-circle" />
+              <Grid item>
+                <Icon className="fab fa-github" />
+              </Grid>
             </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <AppBar
-        component="div"
-        className={classes.secondaryBar}
-        color="primary"
-        position="static"
-        elevation={0}
-      >
-        <Toolbar>
-          <Grid container alignItems="center" spacing={1}>
-            <Grid item xs>
-              <Typography color="inherit" variant="h5" component="h1">
-                Authentication
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Icon className="fab fa-github" />
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  );
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+    );
+  }
 }
 
 Header.propTypes = {
@@ -108,4 +108,11 @@ Header.propTypes = {
   onDrawerToggle: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(Header);
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(withStyles(styles)(Header));
