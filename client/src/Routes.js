92836f016eca2cfe0a520e38adde2d9bb0b04945
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-10-15 07:42:24 +1100

    feat(React/PrivateRoute): add PrivateRoute

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,19 +1,26 @@
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
+import PrivateRoute from "./components/common/PrivateRoute";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import { default as UsersList } from "./components/Users/List";
 import { default as TeamsList } from "./components/Teams/List";
+import { default as TeamCreate } from "./components/Teams/Create";
 
 class Routes extends Component {
   render() {
     return (
       <React.Fragment>
-        <Route exact path="/" component={UsersList} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/users" component={UsersList} />
-        <Route exact path="/teams" component={TeamsList} />
+        <Router>
+          <Route exact path="/" component={UsersList} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/teams" component={TeamsList} />
+          <Switch>
+            <PrivateRoute exact path="/teams/create" component={TeamCreate} />
+          </Switch>
+        </Router>
       </React.Fragment>
     );
   }
