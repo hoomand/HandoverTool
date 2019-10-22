9775de89596269c3eead8913429b4dcf8257cd48
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-10-22 13:23:56 +1100

    feat(Handovers/Create): implement SelectFieldGroup to show valid statuses

--- client/src/components/Handovers/Create.js
+++ client/src/components/Handovers/Create.js
@@ -13,15 +13,17 @@ import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 
+import isEmpty from "../../utils/is-empty";
 import { setHeaderTitle } from "../../redux/actions/headerActions";
 import { getTeams } from "../../redux/actions/teamActions";
+import { getConfigs } from "../../redux/actions/configActions";
 import { IconButton } from "@material-ui/core";
 
 class Create extends Component {
   state = {
     handingOverTeam: "",
     handedOverTeam: "",
-    items: [],
+    items: [{ status: "" }],
     errors: {}
   };
   UNSAFE_componentWillReceiveProps(nextProps) {
@@ -50,6 +52,7 @@ class Create extends Component {
   componentDidMount() {
     this.props.setHeaderTitle("Handovers - New Handover");
     this.props.getTeams();
+    this.props.getConfigs();
 
     if (!this.props.auth.isAuthenticated) {
       this.props.history.push("/login");
@@ -63,8 +66,14 @@ class Create extends Component {
   render() {
     const { classes, teams } = this.props;
     const { errors } = this.state;
-    let teamNames = [];
-    teams.data.forEach(team => teamNames.push(team.name));
+
+    const teamNames = {};
+    teams.data.forEach(team => (teamNames[team.name] = team.name));
+
+    const { handoverItem } = this.props.configs.data;
+    const validStatuses = !isEmpty(handoverItem)
+      ? handoverItem.validStatuses
+      : {};
 
     return (
       <Container component="main" maxWidth="lg">
@@ -122,7 +131,16 @@ class Create extends Component {
                 </AppBar>
                 <Grid container alignItems="center">
                   <Grid item xs={11}>
-                    Status, Link, Description
+                    <SelectFieldGroup
+                      id="itemStatus-0"
+                      label="Issue Status"
+                      name="items"
+                      value={this.state.items}
+                      options={validStatuses}
+                      helperText="ticket status"
+                      onChange={this.onChange}
+                      error={errors.items}
+                    />
                   </Grid>
                   <Grid item xs alignSelf="flex-end">
                     <IconButton color="inherit" onClick={this.addItem}>
@@ -158,16 +176,19 @@ Create.propTypes = {
   errors: PropTypes.object.isRequired,
   history: PropTypes.object,
   getTeams: PropTypes.func.isRequired,
-  teams: PropTypes.object.isRequired
+  teams: PropTypes.object.isRequired,
+  getConfigs: PropTypes.func.isRequired,
+  configs: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   errors: state.errors,
   auth: state.auth,
-  teams: state.teams
+  teams: state.teams,
+  configs: state.configs
 });
 
 export default connect(
   mapStateToProps,
-  { setHeaderTitle, getTeams }
+  { setHeaderTitle, getTeams, getConfigs }
 )(withRouter(withStyles(listStyles)(Create)));
