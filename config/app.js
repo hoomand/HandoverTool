9c27371ea12e44d7104307b5b1b7f5ff5e8a4ee7
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-10-23 14:07:21 +1100

    ref(Config): rewrite item status messages in shorter sentences

--- config/app.js
+++ config/app.js
@@ -4,9 +4,8 @@ module.exports = {
     validStatuses: {
       fresh: "fresh issue, no investigation done yet",
       investigated: "did some investigation, but couldn't fully diagnose",
-      diagnosed: "fully diagnosed, clear steps provided to rectify the issue",
-      monitor:
-        "diagnosed actions were performed, only monitoring needed to confirm issue is rectified"
+      diagnosed: "clear steps provided to rectify the issue",
+      monitor: "diagnosed actions were performed, only monitor to verify"
     },
     mustBeValidURL: true
   }
