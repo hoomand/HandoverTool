5f9241b0d0bf774ac796fd35accd19d8e69e261e
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-12-07 14:25:38 +1100

    conf(dynamoose): connect to AWS DDB via passed env variables

--- db/index.js
+++ db/index.js
@@ -1,16 +1,40 @@
 const client = require("dynamoose");
-let dynamoDBEndPoint = "";
+const isEmpty = require("../validation/is-empty");
+
+let message = "Dynamo running in ";
+const { AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION } = process.env;
+
 switch (process.env.NODE_ENV) {
   case "test":
-    dynamoDBEndPoint = "http://localhost:8010";
+    console.log(message + "test");
+    client.local("http://localhost:8010");
     break;
-  case "prod":
-    dynamoDBEndPoint = "http://aws.amazon.com/someURL";
+  case "production":
+    console.log(message + "production");
+    if (
+      isEmpty(AWS_ACCESS_KEY) ||
+      isEmpty(AWS_SECRET_KEY) ||
+      isEmpty(AWS_REGION)
+    ) {
+      console.error(
+        "AWS environment variables must not be empty for production environment: AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION "
+      );
+      process.exit(17);
+    }
+    client.AWS.config.update({
+      accessKeyId: AWS_ACCESS_KEY,
+      secretAccessKey: AWS_SECRET_KEY,
+      region: AWS_REGION
+    });
+    client.setDefaults({
+      create: true,
+      prefix: "hot-"
+    });
     break;
-  case "dev":
+  case "development":
   default:
-    dynamoDBEndPoint = "http://localhost:8000";
+    console.log(message + "development");
+    client.local("http://localhost:8000");
 }
-client.local(dynamoDBEndPoint);
 
 module.exports = client;
