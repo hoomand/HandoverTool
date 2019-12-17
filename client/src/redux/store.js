47f1065ebd50530a4f67f84dad8fb33fc30306f0
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-12-17 19:33:22 +1100

    fix(redux): use correct composer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,13 +4,15 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 const middleWare = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+// Middleware you want to use in production:
+const enhancer = composeEnhancers(applyMiddleware(...middleWare));
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
