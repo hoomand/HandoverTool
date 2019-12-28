feffa71df818f026863dd2c133f969ecfb8f6737
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-12-28 16:02:58 +1100

    conf(express): serve static react files in production

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const teams = require("./routes/api/teams");
@@ -20,5 +21,18 @@ app.use("/api/teams", teams);
 app.use("/api/handovers", handovers);
 
 app.use("/api/configs", configs);
+
+//============================================================//
+/* PRODUCTION CONFIG */
+//============================================================//
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("client", "build", "index.html"))
+  );
+}
 
 module.exports = app;
